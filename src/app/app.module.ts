1f0607dcc8f194f9d63c30a9315baa70b6adb7e8
JohnKhong <x3johnkhong@hotmail.com>
2022-07-29 09:51:54 +1000

    06_02b Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpBackend, HttpXhrBackend } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { MediaItemComponent } from './media-item.component';
@@ -8,12 +9,14 @@ import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
 import { lookupListsToken, lookupLists } from './providers';
+import { MockXHRBackend } from './mock-xhr-backend';
 
 
 @NgModule({
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
@@ -24,7 +27,8 @@ import { lookupListsToken, lookupLists } from './providers';
     MediaItemFormComponent
   ],
   providers: [
-    { provide: lookupListsToken, useValue: lookupLists}
+    { provide: lookupListsToken, useValue: lookupLists},
+    { provide: HttpXhrBackend,  useClass: MockXHRBackend }
   ],
   bootstrap: [
     AppComponent
